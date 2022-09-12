fcefb5bd5b2bf43a5313a0558d371c2d36f9b79d
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-11 19:25:50 -0500

    Implementando formulario reactivo en register.component

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: [ './register.component.css' ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public registerForm = this.fb.group({
+    nombre: [ 'Fernando', [ Validators.required, Validators.minLength(3) ] ],
+    email: [ 'test100@gmail.com', Validators.required ],
+    password: [ '123456', Validators.required ],
+    password2: [ '123456', Validators.required ],
+    terminos: [ false, Validators.required ]
+  });
+  
+  constructor( private fb: FormBuilder ) { }
 
-  ngOnInit(): void {
+  crearUsuario() {
+    console.log( this.registerForm.value );
   }
-
 }
