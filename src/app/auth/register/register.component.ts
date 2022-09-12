8e337c7474e0209d02395fa36cccca83a4b1b6b0
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-11 19:49:29 -0500

    Implementando validaciones a register.component

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -8,17 +8,35 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
 
+  public formSubmitted = false;
+
   public registerForm = this.fb.group({
-    nombre: [ 'Fernando', [ Validators.required, Validators.minLength(3) ] ],
-    email: [ 'test100@gmail.com', Validators.required ],
-    password: [ '123456', Validators.required ],
-    password2: [ '123456', Validators.required ],
+    nombre: [ 'Fer Santi', [ Validators.required, Validators.minLength(3) ] ],
+    email: [ 'test100@gmail.com', [ Validators.required, Validators.email ] ],
+    password: [ '', Validators.required ],
+    password2: [ '', Validators.required ],
     terminos: [ false, Validators.required ]
   });
   
   constructor( private fb: FormBuilder ) { }
 
   crearUsuario() {
+    this.formSubmitted = true;  
     console.log( this.registerForm.value );
+
+    this.registerForm.valid ? console.log( 'Posteando formulario' ) 
+                            : console.log( 'Formulario no es correcto' );
+  }
+
+  campoNoValido( campo: string ): boolean {
+    if( this.registerForm.get( campo )?.invalid && this.formSubmitted ) { 
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  aceptaTerminos() {
+    return !this.registerForm.get('terminos')?.value && this.formSubmitted;
   }
 }
